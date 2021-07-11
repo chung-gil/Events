bbf247220bc0b92f11040c281d7b2caa1c87853b
chunggil han <changgilhan@gmail.com>
2021-07-10 21:56:42 -0400

    Add project files.

---
+++ ChainedEvents/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ChainedEvents
+{
+    public delegate void myEventHandler(string value);
+
+    class EventPublisher
+    {
+        private string theVal;
+
+        public event myEventHandler valueChanged;
+        public event EventHandler<ObjChangeEventArgs> objChanged;
+
+        public string Val
+        {
+            set
+            {
+                this.theVal = value;
+                this.valueChanged(theVal);
+                this.objChanged(this, new ObjChangeEventArgs() { propChanged = "Val" });
+            }
+        }
+    }
+
+    class ObjChangeEventArgs : EventArgs
+    {
+        public string propChanged;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventPublisher obj = new EventPublisher();
+
+            obj.valueChanged += changeListener1;
+            obj.valueChanged += changeListener2;
+
+            obj.valueChanged += delegate (string s)
+            {
+                Console.WriteLine($"This came from the anonymous handler! {s}");
+            };
+
+            obj.objChanged += delegate (object sender, ObjChangeEventArgs e)
+            {
+                Console.WriteLine($"{sender.GetType()} had the '{e.propChanged}' property changed");
+            };
+
+            string str;
+            do
+            {
+                Console.WriteLine("Enter a value: ");
+                str = Console.ReadLine();
+                if (!str.Equals("exit"))
+                {
+                    obj.Val = str;
+                }
+            }
+            while (!str.Equals("exit"));
+
+            Console.WriteLine("Goodbye!");
+        }
+
+        static void changeListener1(string value)
+        {
+            Console.WriteLine($"The value changed to {value}");
+        }
+
+        static void changeListener2(string value)
+        {
+            Console.WriteLine($"I also listen to the event, and got {value}");
+        }
+    }
+}
---
+++ CodeChallenge/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CodeChallenge
+{
+    public delegate void BalanceEventHandler(decimal theValue);
+
+    class PiggyBank
+    {
+        private decimal m_bankBalance;
+        public event BalanceEventHandler balanceChanged;
+
+        public decimal theBalance
+        {
+            set
+            {
+                m_bankBalance = value;
+                balanceChanged(value);
+            }
+            get
+            {
+                return m_bankBalance;
+            }
+        }
+    }
+
+    class BalanceLogger
+    {
+        public void balanceLog(decimal amount)
+        {
+            Console.WriteLine($"The balance amount is {amount}");
+        }
+    }
+
+    class BalanceWatcher
+    {
+        public void balanceWatch(decimal amount)
+        {
+            if(amount > 500.0m)
+            {
+                Console.WriteLine($"You reached your savings goal! You have {amount}");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PiggyBank pb = new PiggyBank();
+            BalanceLogger bl = new BalanceLogger();
+            BalanceWatcher bw = new BalanceWatcher();
+
+            pb.balanceChanged += bl.balanceLog;
+            pb.balanceChanged += bw.balanceWatch;
+
+            string theStr = "";
+            
+            do
+            {
+                Console.WriteLine("How much to deposit?");
+
+                theStr = Console.ReadLine();
+                if(!theStr.Equals("eixt"))
+                {
+                    decimal newVal = 0m;
+                    if (decimal.TryParse(theStr, out newVal))
+                    {
+                        pb.theBalance += newVal;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter valid number");
+                    }
+                }
+            }
+            while (theStr != "exit");
+        }
+    }
+}
